9d3433c36645f42dbdd4f5d644a6c99735bb3cea
ARJUN2331 <89044218+arjunram2020@users.noreply.github.com>
2025-06-04 13:11:09 -0400

    Add files via upload

---
+++ producers.py
@@ -0,0 +1,15 @@
+# Importing necessary libraries for producer.py - from data.csv to Kafka topic
+from kafka import KafkaProducer
+import json
+import csv
+# Create a Kafka producer to send data to the Kafka topic
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+with open('/Users/arjunramakrishnan/Downloads/NYSE_shortened.csv') as file:
+    #Read the CSV file using DictReader and handled the delimeter using ;
+    reader = csv.DictReader(file, delimiter=';')
+    #sent each user (row) of the CSV file to Kafka topic
+    for row in reader:
+        producer.send(topic = 'earnings_topic', value = row)
+        producer.flush()
+
