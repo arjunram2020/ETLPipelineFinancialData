360eb932a33f3685211f19815183ed5130a4f7e2
ARJUN2331 <89044218+arjunram2020@users.noreply.github.com>
2025-06-09 11:34:00 -0400

    producers.py

--- producers.py
+++ producers.py
@@ -5,11 +5,13 @@ import csv
 # Create a Kafka producer to send data to the Kafka topic
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-with open('/Users/arjunramakrishnan/Downloads/NYSE_20250603.csv') as file:
+with open('/Users/arjunramakrishnan/Downloads/NYSE_shortened.csv') as csvfile:
     #Read the CSV file using DictReader and handled the delimeter using ;
-    reader = csv.DictReader(file, delimiter=';')
+    reader = csv.DictReader(csvfile, delimiter=',')
     #sent each user (row) of the CSV file to Kafka topic
     for row in reader:
+        print(row)
         producer.send(topic = 'earnings_topic', value = row)
         producer.flush()
+
 
